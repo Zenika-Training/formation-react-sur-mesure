11bbe53e1f61f31177cead165e20b8b5fa3f9fce
Lokymil <arrestierchristophe@gmail.com>
2021-02-01 16:25:55 +0100

    Login page + user infos context + auth api request

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import Login from "./components/login/Login";
 import TodoForm from "./components/todo/form/TodoForm";
 import TodoList from "./components/todo/list/TodoList";
 import "./App.scss";
+import { UserInfosProvider } from "./components/userInfos/useUserInfos";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route>
-          <Layout>
-            <Switch>
-              <Route exact path="/" component={TodoList} />
-              <Route exact path="/add" component={TodoForm} />
-              <Route exact path="/update/:id" component={TodoForm} />
-              <Redirect to="/" />
-            </Switch>
-          </Layout>
-        </Route>
-      </Switch>
+      <UserInfosProvider>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route>
+            <Layout>
+              <Switch>
+                <Route exact path="/" component={TodoList} />
+                <Route exact path="/add" component={TodoForm} />
+                <Route exact path="/update/:id" component={TodoForm} />
+                <Redirect to="/" />
+              </Switch>
+            </Layout>
+          </Route>
+        </Switch>
+      </UserInfosProvider>
     </BrowserRouter>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import useAuthApi from "../api/useAuthApi";
+import useUserInfos from "../userInfos/useUserInfos";
 import "./Header.scss";
 
 function Header() {
+  const api = useAuthApi();
+  const { userInfos, addInfos } = useUserInfos();
+
+  useEffect(() => {
+    api
+      .get("/users/current")
+      .then(({ data }) => addInfos(data))
+      .catch((e) => console.error(e));
+  }, []);
+
   return (
     <div className="header__wrapper">
-      <div className="header__greeting">Bonjour $UTILISATEUR$</div>
-      <div className="header__karma">Karma: $POINTS$</div>
+      <div className="header__greeting">Bonjour {userInfos.firstName}</div>
+      <div className="header__karma">Karma: {userInfos.karma}</div>
     </div>
   );
 }
